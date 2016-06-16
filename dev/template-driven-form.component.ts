101c23ec0c20ba5ad6fa9c155617dbd771bd7484
Hashrocket <jasshultz@gmail.com>
2016-06-16 14:51:18 -0600

    template driven angular 2 form

--- dev/template-driven-form.component.ts
+++ dev/template-driven-form.component.ts
@@ -7,23 +7,32 @@ import {Component} from '@angular/core';
         <form (ngSubmit)="onSubmit(f)" #f="ngForm">
             <div>
                 <label for="mail">Mail</label>
-                <input ngControl="email" type="text" id="mail" required>
+                <input ngControl="email" type="text" id="mail" required #email="ngForm">
+                <span class="validation-error" *ngIf="!email.valid">Not Valid</span>
             </div>
             <div>
                 <label for="password">Password</label>
-                <input ngControl="password" type="text" id="password" required>
+                <input ngControl="password" type="text" id="password" required #password="ngForm">
+                <span class="validation-error" *ngIf="!password.valid">Not Valid</span>
             </div>
             <div>
                 <label for="confirm-password">Confirm Password</label>
-                <input ngControl="confirm-password" type="text" id="confirm-password" required>
+                <input ngControl="confirm-password" type="text" id="confirm-password" required #passwordConfirm="ngForm">
+                <span class="validation-error" *ngIf="!passwordConfirm.valid">Not Valid</span>
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" [disabled]="!f.valid || password.value != passwordConfirm.value">Submit</button>
             
 </form>
+<h2>You submitted</h2>
+<div>Mail: {{user.mail}}</div>
+<div>Password: {{user.password}}</div>
     `,
 })
 export class TemplateDrivenFormComponent {
+    user = {mail: '', password: ''};
     onSubmit(form) {
         console.log(form.value)
+        this.user.mail = form.value['email'];
+        this.user.password = form.controls['password'].value;
     }
 }
