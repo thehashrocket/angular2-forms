514284837a864e3bcd0559d9fc0a2fe79c8a6635
Hashrocket <jasshultz@gmail.com>
2016-06-20 08:18:43 -0600

    form submitting

--- dev/data-driven-form.component.ts
+++ dev/data-driven-form.component.ts
@@ -1,23 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, ControlGroup, Validators} from "@angular/common";
 
 @Component({
     selector: 'my-data-driven',
     template: `
         <h2>Sign-up form</h2>
-        <form (ngSubmit)="onSubmit()">
+        <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
             <div>
                 <label for="mail">Mail</label>
-                <input type="text" id="mail" required>
+                <input [ngFormControl]="myForm.controls['email']" type="text" id="mail" required>
                 <span class="validation-error" >Not Valid</span>
             </div>
             <div>
                 <label for="password">Password</label>
-                <input type="text" id="password" required >
+                <input [ngFormControl]="myForm.controls['password']" type="text" id="password" required >
                 <span class="validation-error" >Not Valid</span>
             </div>
             <div>
                 <label for="confirm-password">Confirm Password</label>
-                <input type="text" id="confirm-password" required >
+                <input [ngFormControl]="myForm.controls['confirmPassword']" type="text" id="confirm-password" required >
                 <span class="validation-error" >Not Valid</span>
             </div>
             <button type="submit">Submit</button>
@@ -28,11 +29,21 @@ import {Component} from '@angular/core';
 <div>Password: {{user.password}}</div>
     `,
 })
-export class DataDrivenFormComponent {
+export class DataDrivenFormComponent implements OnInit {
+    myForm: ControlGroup;
     user = {mail: '', password: ''};
+
+    constructor(private _formBuilder: FormBuilder) {}
+
     onSubmit(form) {
-        console.log(form.value)
-        this.user.mail = form.value['email'];
-        this.user.password = form.controls['password'].value;
+        console.log(this.myForm);
+    }
+
+    ngOnInit():any {
+        this.myForm = this._formBuilder.group({
+            'email': ['', Validators.required],
+            'password': ['', Validators.required],
+            'confirmPassword': ['', Validators.required]
+        });
     }
 }
